c1660ea1f2935eff001d2eaaa11abeb848eb21cd
Martin <hzy@acmsmu.cn>
2020-04-27 11:45:16 +0800

    更新README

--- cn/acmsmu/FG/WelcomeMember.py
+++ cn/acmsmu/FG/WelcomeMember.py
@@ -15,10 +15,11 @@ async def welcome(session:NoticeSession):
     print('群成员增加')
     print(str(session.event.group_id))
     if session.event.group_id == 123456:
-        msg = '[CQ:at,qq='+str(session.event.user_id)+']\n欢迎进入哈哈笑的科幻群\n'\
+        msg = '[CQ:at,qq='+str(session.event.user_id)+']\n欢迎加入哈哈笑的科幻群\n'\
             '我是第五代超级计算机FG\n'\
             '我为你准备了一份入群礼物[CQ:face,id=178]\n'+url1+'\n'+\
             '放心，不是什么奇怪的东西，请放心点击~\n想要获取哈哈笑老师的全部有声书，可以艾特我，并在后面附上【有声书】即可\n'\
+            "若想查找哈哈笑老师的某一本有声小说，可以艾特我，并在后面附上【找书 你想找的书名】即可，我会告诉你在线收听平台和下载地址(如果有)，返回下载地址时，我需要连接网络，可能速度会有些慢，请您谅解~\n"\
             '祝你在群里玩的愉快~'
         print(msg) 
         await session.send(msg)
